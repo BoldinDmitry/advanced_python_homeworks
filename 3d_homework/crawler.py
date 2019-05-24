25f3bf114b35dbb87d744e8301b508fbfd9c06f0
Dmitry <boldin.dmitry@gmail.com>
2019-05-24 20:50:53 +0300

    3d_homework timeout exception

--- 3d_homework/crawler.py
+++ 3d_homework/crawler.py
@@ -1,10 +1,18 @@
 import asyncio
 import re
 
+from asyncio import TimeoutError
 from aioelasticsearch import Elasticsearch
 from aioelasticsearch import exceptions as elastic_exceptions
 from aiohttp import ClientSession, TCPConnector
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+
+@dataclass
+class Link:
+    link: str
+    errors_count: int
 
 
 class Crawler:
@@ -12,6 +20,8 @@ class Crawler:
     DOMAIN = "habr.com"
 
     LINKS = asyncio.Queue()
+    LINKS_FOR_RPS = asyncio.Queue()
+
     ALREADY_WAS = []
     LINKS_COUNTER = 0
 
@@ -30,38 +40,56 @@ class Crawler:
 
     async def fetch(self, session):
         while self.LINKS_COUNTER < 1000:
-            url = await self.LINKS.get()
-            async with session.get(url) as response:
-                body = await response.read()
+            url_obj = await self.LINKS.get()
+            url = url_obj.link
+            try:
+                async with session.get(url, timeout=1) as response:
+                    body = await response.read()
 
-                soup = BeautifulSoup(body, "html.parser")
-                await self.make_record_to_es(url, soup)
+                    soup = BeautifulSoup(body, "html.parser")
+                    await self.make_record_to_es(url, soup)
 
-                for a in soup.findAll(
-                    "a", attrs={"href": re.compile("^http?[s]://.*/$")}
-                ):
-                    link = a.attrs["href"]
-                    if self.DOMAIN in link and link not in self.ALREADY_WAS:
-                        await self.LINKS.put(link)
-                        self.ALREADY_WAS.append(link)
+                    for a in soup.findAll(
+                            "a", attrs={"href": re.compile("^http?[s]://.*/$")}
+                    ):
+                        link = a.attrs["href"]
+                        if self.DOMAIN in link and link not in self.ALREADY_WAS:
+                            link_obj = Link(link=link, errors_count=0)
+                            await self.LINKS_FOR_RPS.put(link_obj)
+                            self.ALREADY_WAS.append(link)
+            except TimeoutError:
+                print("error")
+                if url_obj.errors_count > 1:
+                    pass
+                else:
+                    url_obj.errors_count += 1
+                    await self.LINKS_FOR_RPS.put(url_obj)
 
     async def bound_fetch(self, sem, session):
         async with sem:
             return await self.fetch(session)
 
+    async def rps_counter(self, rps):
+        while True:
+            url = await self.LINKS_FOR_RPS.get()
+
+            await asyncio.sleep(1 / rps)
+            await self.LINKS.put(url)
+
     async def run(self, rps):
-        await self.LINKS.put(self.START_URL)
+        await self.LINKS.put(Link(link=self.START_URL, errors_count=0))
 
         sem = asyncio.Semaphore(20)
 
-        connector = TCPConnector(limit=rps, verify_ssl=False)
+        connector = TCPConnector(verify_ssl=False)
 
         async with ClientSession(connector=connector) as session:
-
             tasks = []
+
             for _ in range(5):
-                task = asyncio.ensure_future(self.bound_fetch(sem, session))
-                tasks.append(task)
+                tasks.append(asyncio.ensure_future(self.bound_fetch(sem, session)))
+
+            tasks.append(asyncio.ensure_future(self.rps_counter(rps)))
 
             responses = asyncio.gather(*tasks)
             await responses
